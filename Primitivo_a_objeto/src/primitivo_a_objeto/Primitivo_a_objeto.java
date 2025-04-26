1a8abb5200aed7c4712a897b811a7815809598b1
Esteban Ricardo <juanrz2021@gmail.com>
2025-04-25 19:36:24 -0500

    1.Inicio

---
+++ Primitivo_a_objeto/src/primitivo_a_objeto/Primitivo_a_objeto.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package primitivo_a_objeto;
+
+/**
+ *
+ * @author juanr
+ */
+public class Primitivo_a_objeto {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
